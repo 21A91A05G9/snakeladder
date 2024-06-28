7e831a4b2c24ed47113875d3abea947bce8369bf
21A91A05G9 <21a91a05g9@aec.edu.in>
2024-06-28 14:43:46 +0530

    adding port

--- database.php
+++ database.php
@@ -3,12 +3,11 @@ $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";  // Your MySQL password
 $db_name = "snake&ladder";
-$db_port = 3306;  // Default MySQL port on Windows
 
 $conn = null;
 
 try {
-    $conn = new mysqli($db_server, $db_user, $db_pass, $db_name, $db_port);
+    $conn = new mysqli($db_server, $db_user, $db_pass, $db_name, null, '/var/run/mysqld/mysqld.sock');
     if ($conn->connect_error) {
         die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
     }
